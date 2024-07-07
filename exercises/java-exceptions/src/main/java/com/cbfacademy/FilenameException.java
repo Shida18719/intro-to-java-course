922a9e0e971aca4f278eafcc69b2d0d145cdab91
Rashidat <rawsheedah@yahoo.com>
2024-07-07 14:34:21 +0100

    Create FilenameException class to call constructor of parent Exception

---
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FilenameException.java
@@ -0,0 +1,10 @@
+package com.cbfacademy;
+
+public class FilenameException extends Exception{
+
+    public FilenameException(String message) {
+        // Call constructor of parent Exception
+        super(message);
+    }
+    
+}
