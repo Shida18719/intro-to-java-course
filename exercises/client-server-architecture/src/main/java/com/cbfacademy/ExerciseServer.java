900e27fe2c309e0e88c993d9f1ea3945ed097395
Rashidat <rawsheedah@yahoo.com>
2024-07-30 19:01:31 +0100

    Modify java imports

--- exercises/client-server-architecture/src/main/java/com/cbfacademy/ExerciseServer.java
+++ exercises/client-server-architecture/src/main/java/com/cbfacademy/ExerciseServer.java
@@ -1,13 +1,16 @@
 package com.cbfacademy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.net.ServerSocket;
+// import java.net.Socket;
 
+
+import java.io.*;
+import java.net.*;
 
 public class ExerciseServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
             // ServerSocket object
             ServerSocket serverSocket = new ServerSocket(4040);
@@ -19,9 +22,9 @@ public class ExerciseServer {
 
                 BufferedReader reader = new BufferedReader
                 (new InputStreamReader(socketObj.getInputStream()))) {
-                    String inputLine = reader.readLine();
+                    String message = reader.readLine();
                 
-                    System.out.println("Received message from client: " + inputLine);  
+                    System.out.println("Received message from client: " + message);  
                   }
 
                   serverSocket.close();
@@ -29,8 +32,8 @@ public class ExerciseServer {
             }
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
-            // e.printStackTrace();
+            // System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
     
