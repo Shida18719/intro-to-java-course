71cde4eb00422beb0ddaeca04354ac8c412679d1
Rashidat <rawsheedah@yahoo.com>
2024-08-14 14:19:42 +0100

    Use the IoC Container to manage dependencies with container.register and .get to provide a concrete instance of the interface

--- exercises/software-design/src/main/java/com/cbfacademy/App.java
+++ exercises/software-design/src/main/java/com/cbfacademy/App.java
@@ -2,8 +2,17 @@ package com.cbfacademy;
 
 public class App {
     public static void main(String[] args) {
-        // DiceGame game = GameFactory.create();
-        Game game = GameFactory.create();
+        SimpleContainer container = initializeContainer();
+        // Player player1 = PlayerFactory.create();
+        // Player player2 = PlayerFactory.create();
+        // Remove PlayerFactory.create() - Retrieve the Player instances from the container
+        
+
+        //  Game game = GameFactory.create(player1, player2); // Modified to Game rather than DiceGame
+
+        // Replace GameFactory.create() - Retrieve the Game instance from the container
+        Game game = container.get(Game.class); 
+
         String winner = game.play();
 
         if (winner == null) {
@@ -12,4 +21,17 @@ public class App {
             System.out.println(winner + " wins!");
         }
     }
+
+    private static SimpleContainer initializeContainer() {
+        SimpleContainer container = new SimpleContainer();
+
+        // Register mappings for any required interfaces with their concrete implementations
+
+        container.register(Game.class, DiceGame.class);
+
+        container.register(Player.class, DicePlayer.class);
+        return container;
+    }
+
+
 }
