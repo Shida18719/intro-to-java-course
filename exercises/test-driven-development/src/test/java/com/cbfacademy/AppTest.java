fe4a6903c28ebbd98d4e35f6d46f6b94e7ba4cf6
Rashidat <rawsheedah@yahoo.com>
2024-08-21 21:44:48 +0100

    Conduct test for red and green

--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,5 +1,7 @@
 package com.cbfacademy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,31 @@ public class AppTest {
         final App app = new App();
 
         assertThat(app, is(notNullValue()));
+    }
+
+
+    @Test
+    @DisplayName("print FizzBuzz for multiples of 15")
+    public void printFizzBuzz() {
+
+        final String result = FizzBuzz.get(15);
+        assertEquals(result, "FizzBuzz");
+    }
+
+
+    @Test
+    @DisplayName("print Fizz for multiples is 3")
+    public void printFizz() {
+
+        final String result = FizzBuzz.get(9);
+        assertEquals(result, "Fizz");
+    }
+
+    @Test
+    @DisplayName("print Buzz for multiples of 5")
+    public void printBuzz() {
+
+        final String result = FizzBuzz.get(10);
+        assertEquals(result, "Buzz");
     }
 }
