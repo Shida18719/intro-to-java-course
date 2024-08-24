d6f53f41cc999125c43c5e0b9cca5951fcca4c06
Rashidat <rawsheedah@yahoo.com>
2024-08-24 16:14:26 +0100

    Complete test for refactoring

--- exercises/test-driven-development/src/test/java/com/cbfacademy/YearTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/YearTest.java
@@ -6,13 +6,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class YearTest {
 
-    //Leap Year Checker Test
+    // Leap Year Checker Test
+    
     // @Test
     // @DisplayName("Year divisible by 400 return true")
 
@@ -58,7 +60,7 @@ public class YearTest {
 
     static Stream<Arguments> LeapYearIOProvider() {
         return Stream.of(
-            Arguments.of(1700, true),
+            Arguments.of(1700, false),
             Arguments.of(1800, false),
             Arguments.of(1900, false),
             Arguments.of(2000, true),
@@ -67,7 +69,12 @@ public class YearTest {
             Arguments.of(2003, false),
             Arguments.of(2004, true),
             Arguments.of(2005, false),
-            Arguments.of(2006, true)
+            Arguments.of(2006, false),
+            Arguments.of(2007, false),
+            Arguments.of(2008, true),
+            Arguments.of(2009, false),
+            Arguments.of(2010, false),
+            Arguments.of(2011, false)
         );
     }
 
@@ -79,7 +86,6 @@ public class YearTest {
         Year isLeap = new Year(n);
         assertThat(isLeap.isLeapYear(), is(expected));
     }
-
 }
     
-}
+
