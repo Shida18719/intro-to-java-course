6a13fe83af4d9e0d388f8171883d0b6afd416262
Rashidat <rawsheedah@yahoo.com>
2025-01-21 18:54:57 +0000

    Add comment to methods

--- exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/Account.java
+++ exercises/object-oriented-programming/accounts/src/main/java/com/cbfacademy/accounts/Account.java
@@ -7,33 +7,31 @@ public class Account {
     protected int accountNumber;
     protected double balance;
 
-    // constructor that accepts parameters representing the account number and balance
+    // Constructor that accepts parameters representing the account number and balance
     public Account(int accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
+    // Returns the current account balance
     public double getBalance() {
         return this.balance;
     }
-        
+    
+    // Returns the account number
     public int getAccountNumber() {
         return this.accountNumber;
     }
-        
+    
+    // Deposits funds to the account and returns the new balance
     public double deposit(double amount) {
         this.balance += amount;
         return this.balance;
     }
     
-    // public double withdraw(double requested) {
-    //     if (requested < 0 || this.balance < requested) {
-    //         return 0;
-    //     } else {
-    //         this.balance -= requested;
-    //         return requested;
-    //     }
-        
+    
+    // Withdraws funds from the account and returns the requested amount or 0,
+    // if the account has an insufficient balance
     public double withdraw(double requested) {
         if (this.balance < requested || requested < 0) {
           return 0;
